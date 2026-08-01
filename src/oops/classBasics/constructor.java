53274a0d019c1677152750e45d5cb20d5aeb4bf4
Ankit Dhankar <adhankar1604@gmail.com>
2026-08-01 12:20:01 +0530

    add comments for notes

--- src/oops/classBasics/constructor.java
+++ src/oops/classBasics/constructor.java
@@ -5,20 +5,30 @@ class Student1{
     int age;
     String name;
     String college;
-    //Default constructor
+
+    /*Default constructor : if we dont create it java automatically creates the default construtor
+      which will assign default vaules to instance variables
+    * */
     Student1(){
         rollNo=1;
         age=18;
         name="NA";
         college="NA";
     }
+
     //parameterized constructor
     Student1(int rollNo, String name,int age,String college){
+
+        /*this keyword refers to current object */
         this.name=name;
         this.rollNo=rollNo;
         this.age=age;
         this.college=college;
     }
+    /*NOTE : if we make parameterized constructor and we want to create the object with default constructor
+    than it will give the error because java will not create it automatically now so to do this we have to
+    create the constructor by ourself (overload constructors) so that we can use it to create the object
+     */
 
     public void markAttendance(){
         System.out.println("Attendance marked by "+name);
